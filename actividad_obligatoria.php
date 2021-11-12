3f5b4914ddf67b7f053f7161ffb2574ee9054df6
Elumenel <fcastellarin@yahoo.com>
2021-11-12 08:46:38 -0300

    subiendo archivo actividad php

---
+++ actividad_obligatoria.php
@@ -0,0 +1,12 @@
+<?php
+  $nombre = "Florencia";
+  $apellido = "Castellarin";
+  $edad = 37;
+  $hobbies = ["leer","cantar","viajar"];
+  $editorCodigo = "Atom";
+  $oS = "Windows 7 Ultimate SP 1";
+
+  echo "Hola! Mi nombre es ".$nombre." ".$apellido." y tengo ".$edad." años.<br>";
+  echo "Mis hobbies son ".$hobbies[0].", ".$hobbies[1]." y ".$hobbies[2].".<br>";
+  echo "Mi sistema operativo es ".$oS." y mi editor de código preferido es ".$editorCodigo.".";
+?>
